5d7fd08b0fd45aef2b26603c627a21933ce78859
Theodora2000 <xsimont@stuba.sk>
2021-03-14 16:44:10 +0100

    linked list

--- src/main/java/sk/stuba/uim/fei/oop/Main.java
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -1,14 +1,20 @@
 package sk.stuba.uim.fei.oop;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
-        var absolvent = new Graduate("Ing.","Teodora", "Simon");
+       var zoznam = new LinkedList<>();
+       zoznam.add(new NoZnamy());
+       zoznam.add(new Graduate("Ing.","Teodora", "Simon"));
+       zoznam.add(1, new NoZnamy());
+       System.out.println(zoznam.size());
+       zoznam.add(new NoZnamy());
+       zoznam.remove(2);
 
-        absolvent.setFriends(new Graduate("Ing.","Sergej", "Vidakovic"), new Graduate("Ing.","Ivona", "Bires"));
-        System.out.println(absolvent.akoText());
-        System.out.println(absolvent.akoTextSTitulom());
-        System.out.println(absolvent);
+
     }
 
 }
---
+++ src/main/java/sk/stuba/uim/fei/oop/NoZnamy.java
@@ -0,0 +1,8 @@
+package sk.stuba.uim.fei.oop;
+
+public class NoZnamy implements Znamy{
+    @Override
+    public String getZnamy() {
+        return "Nemam znamych";
+    }
+}
