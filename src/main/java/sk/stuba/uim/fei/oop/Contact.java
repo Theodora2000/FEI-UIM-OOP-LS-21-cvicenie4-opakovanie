77ed1addefe9078e77bdc6a427f9143bd43a26f8
Theodora2000 <xsimont@stuba.sk>
2021-03-14 07:50:01 +0100

    var variables

---
+++ src/main/java/sk/stuba/uim/fei/oop/Contact.java
@@ -0,0 +1,14 @@
+package sk.stuba.uim.fei.oop;
+
+public class Contact {
+    //atributy
+    private String firstname;
+    private String surname;
+
+    Contact[] friends;
+    //konstruktor
+    public Contact(String firstname,String surname) {
+        this.firstname = firstname;
+        this.surname=surname;
+    }
+}
---
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -0,0 +1,10 @@
+package sk.stuba.uim.fei.oop;
+
+public class Main {
+    public static void main(String[] args){
+        //premenna
+        Contact contact = new Contact("Jakub", "Kovar");
+        var contact2 = new Contact("Martin", "Kranec");
+
+    }
+}
