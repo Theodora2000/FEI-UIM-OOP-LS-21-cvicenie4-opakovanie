7a80b7889799b549c9822f4ddc9d8ef0f00a77ce
Theodora2000 <xsimont@stuba.sk>
2021-03-14 08:23:46 +0100

    object references

--- src/main/java/sk/stuba/uim/fei/oop/Contact.java
+++ src/main/java/sk/stuba/uim/fei/oop/Contact.java
@@ -11,4 +11,26 @@ public class Contact {
         this.firstname = firstname;
         this.surname=surname;
     }
+
+    public Contact(String fullName){
+        var parts = fullName.split(" ");
+        firstname = parts[0];
+        surname=parts[1];
+    }
+//staticka metoda
+    public static Contact  parseFromfullName(String fullName){
+        return new Contact(fullName);
+    }
+
+    public void setFriends(Contact... newfriends){
+        this.friends=newfriends;
+    }
+
+    public void setFriendsFromArray(Contact[] newfriends){
+        this.friends=newfriends;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 }
--- src/main/java/sk/stuba/uim/fei/oop/Main.java
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -5,6 +5,16 @@ public class Main {
         //premenna
         Contact contact = new Contact("Jakub", "Kovar");
         var contact2 = new Contact("Martin", "Kranec");
+        var contact3 = new Contact("Tomas Kovacik");
 
+        var contact4 = Contact.parseFromfullName("Gabriel Juhas");
+
+        var contacts1 = new Contact[]{contact2, contact3, contact4};
+        contact.setFriendsFromArray(contacts1);
+
+        contacts1[0].setFirstname("Mato");
+        contact.setFriends(contact4,contact2,contact3);
+        contact3.setFirstname("Kovy");
     }
+
 }
